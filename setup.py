4839ed1df873310f5c877e0dd31497bc23c8766a
richtier <rikatee@gmail.com>
2018-09-13 22:14:55 +0100

    Improve documentation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='command_lifecycle',
     packages=['command_lifecycle'],
-    version='2.1.0',
+    version='2.1.2',
     url='https://github.com/richtier/voice-command-lifecycle',
     license='MIT',
     author='Richard Tier',
